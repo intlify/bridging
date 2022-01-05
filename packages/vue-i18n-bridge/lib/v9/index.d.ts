818551654fc075b226a3e4754497bd515062ab1f
kazuya kawaguchi <kawakazu80@gmail.com>
2022-01-05 15:15:39 +0900

    fix(vue-i18n-bridge): support VueI18n as named export

--- packages/vue-i18n-bridge/lib/v9/index.d.ts
+++ packages/vue-i18n-bridge/lib/v9/index.d.ts
@@ -195,7 +195,7 @@ declare const isVueI18n8: boolean
 declare const isVueI18n9: boolean
 
 export * from 'vue-i18n'
-export { isVueI18n8, isVueI18n9 }
+export { VueI18n, isVueI18n8, isVueI18n9 }
 export default VueI18n
 
 /* eslint-enable @typescript-eslint/no-explicit-any */
