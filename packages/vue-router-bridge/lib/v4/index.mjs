2eacc100cd65a4b0503d8dcff193fe3ed47d1410
kazuya kawaguchi <kawakazu80@gmail.com>
2022-01-07 13:56:02 +0900

    fix(vue-router-bridge): fix cannot default import (#14)
    * fix(vue-router-bridge): fix cannot default import
    
    * fix: unit test

--- packages/vue-router-bridge/lib/v4/index.mjs
+++ packages/vue-router-bridge/lib/v4/index.mjs
@@ -3,5 +3,10 @@ import { useRouter, useRoute } from 'vue-router'
 var isVueRouter3 = false
 var isVueRouter4 = true
 
+// dummy vue-router 3 class
+class VueRouter3 {}
+
 export * from 'vue-router'
 export { useRouter, useRoute, isVueRouter3, isVueRouter4 }
+
+export default VueRouter3
