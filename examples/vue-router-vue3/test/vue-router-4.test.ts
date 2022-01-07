9d1569dc79eaeab378c892977c897244dc80c3c0
kazuya kawaguchi <kawakazu80@gmail.com>
2022-01-07 13:54:14 +0900

    fix: unit test

--- examples/vue-router-vue3/test/vue-router-4.test.ts
+++ examples/vue-router-vue3/test/vue-router-4.test.ts
@@ -1,7 +1,7 @@
 import { default as VueRouter, isVueRouter3, isVueRouter4 } from '@intlify/vue-router-bridge'
 
 test('vue-router-bridge for vue3', () => {
-  assert.isUndefined(VueRouter)
+  assert.isDefined(VueRouter)
   assert.ok(!isVueRouter3)
   assert.ok(isVueRouter4)
 })
