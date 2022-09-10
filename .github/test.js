8229eb63751a2a7a1998677d316f4bd4806f975b
kazuya kawaguchi <kawakazu80@gmail.com>
2022-09-10 18:47:37 +0900

    fix

--- .github/test.js
+++ .github/test.js
@@ -37,8 +37,15 @@ function installDeps(params) {
   const tarball = pack(params)
 
   let installCmd = agent.startsWith('yarn') ? `yarn add` : `${agent} i`
+  console.log('install deps', packageDeps)
   execSync(`${installCmd} ${packageDeps}`, { cwd: dir.test, stdio: 'inherit' })
-  execSync(`${installCmd} ${agent.startsWith('yarn') ? `file:${tarball}` : tarball} --force`, {
+  console.log('install tarball', tarball)
+  // prettier-ignore
+  execSync(`${installCmd} ${agent.startsWith('yarn')
+    ? agent === 'yarn@berry'
+      ? `file:${tarball}`
+      : tarball
+    : tarball} --force`, {
     cwd: dir.test,
     stdio: 'inherit'
   })
